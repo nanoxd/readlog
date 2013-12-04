fe3012b2b211f3a037a4866ec73b672cb0dc4212
Fernando Paredes <nano@fdp.io>
2013-12-03 21:31:31 -0800

    Add validation for Book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book) { Book.new(title: "Arthur",
+                        author: "Marc Brown", 
+                        summary: "Well, the way they make shows is, they make one show.
+                                  She starred in one of the ones that became nothing.") }
+  it "is valid" do
+    expect(book).to be_valid
+  end
+
+  it "is invalid without a title" do
+    book.title = nil
+    expect(book).to be_invalid
+  end
 end
